4eb569aea85577b8f0491901521de3f4f876b21f
omar-24 <alshweny2004@gmail.com>
2024-08-14 17:13:21 +0300

    setup project mvvm

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const finalApp());
+}
+
+class finalApp extends StatelessWidget {
+  const finalApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
